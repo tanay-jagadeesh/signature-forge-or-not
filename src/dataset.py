df35317bc08b9c416ae58bcfd9801086440d9c3e
Tanay <tanayj2107@gmail.com>
2026-01-10 13:21:55 -0500

    finished signaturedataset class

--- src/dataset.py
+++ src/dataset.py
@@ -2,9 +2,15 @@ import torch
 import torchvision
 import torchvision.transforms as T
 import torchvision.transforms as transforms
+from torch import nn
+from torch.utils.data import Dataset
 from PIL import Image
 import matplotlib.pyplot as plt
 import os
+
+
+#Normalize (standardizing pixel values)
+normalize_transform = transforms.Normalize(mean = [0.5], std = [0.5])
 
 #Transform image into tensor
 to_tensor = transforms.ToTensor()
@@ -27,6 +33,7 @@ for filename in filenames:
     resize_img = resize_transform(img_loc)
     transform_img = transform(resize_img)
     tensor_img = to_tensor(transform_img)
+    normalized_img = normalize_transform(tensor_img)
 
 # Process train/fake folder
 filenames = os.listdir("data/train/fake")
@@ -40,6 +47,7 @@ for filename in filenames:
     resize_img = resize_transform(img_loc)
     transform_img = transform(resize_img)
     tensor_img = to_tensor(transform_img)
+    normalized_img = normalize_transform(tensor_img)
 
 # Process test/real folder
 filenames = os.listdir("data/test/real")
@@ -53,6 +61,7 @@ for filename in filenames:
     resize_img = resize_transform(img_loc)
     transform_img = transform(resize_img)
     tensor_img = to_tensor(transform_img)
+    normalized_img = normalize_transform(tensor_img)
 
 # Process test/fake folder
 filenames = os.listdir("data/test/fake")
@@ -66,3 +75,43 @@ for filename in filenames:
     resize_img = resize_transform(img_loc)
     transform_img = transform(resize_img)
     tensor_img = to_tensor(transform_img)
+    normalized_img = normalize_transform(tensor_img)
+
+class SignatureDataset(Dataset):
+    def __init__(self, folder_path):
+        self.image_paths = []
+        self.labels = []
+
+        real_folder = os.path.join(folder_path, "real")
+
+        for filename in os.listdir(real_folder):
+            full_path = os.path.join(real_folder, filename)
+
+            self.image_paths.append(full_path)
+            self.labels.append(0) # 0 is real; 1 is fake
+        
+        fake_folder = os.path.join(folder_path, "fake")
+
+        for filename in os.listdir(fake_folder):
+            full_path = os.path.join(fake_folder, filename)
+
+            self.image_paths.append(full_path)
+            self.labels.append(1)
+
+    def __len__(self):
+        return len(self.image_paths)
+    
+    def __getitem__(self, idx):
+        img_path = self.image_paths[idx]
+        label = self.labels[idx]
+
+        # Load image
+        img = Image.open(img_path)
+
+        # Apply transformations
+        img = resize_transform(img)
+        img = transform(img)
+        img = to_tensor(img)
+        img = normalize_transform(img)
+
+        return img, label
