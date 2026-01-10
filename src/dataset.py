32a9c08f2eb2b173429fc7835b36f432533d8c68
Tanay <tanayj2107@gmail.com>
2026-01-10 13:26:27 -0500

    refactored the code a bit (simplification)

--- src/dataset.py
+++ src/dataset.py
@@ -1,81 +1,17 @@
 import torch
-import torchvision
-import torchvision.transforms as T
 import torchvision.transforms as transforms
-from torch import nn
 from torch.utils.data import Dataset
 from PIL import Image
-import matplotlib.pyplot as plt
 import os
 
 
-#Normalize (standardizing pixel values)
-normalize_transform = transforms.Normalize(mean = [0.5], std = [0.5])
-
-#Transform image into tensor
-to_tensor = transforms.ToTensor()
-
-# Create a transform that resizes images to 224x224
-resize_transform = T.Resize((224, 224))
-
-#Create a transform to make image grayscale
-transform = transforms.Grayscale()
-
-# Process train/real folder
-filenames = os.listdir("data/train/real")
-folder = "data/train/real"
-
-for filename in filenames:
-    full_path = os.path.join(folder, filename)
-    print(full_path)
-
-    img_loc = Image.open(full_path)
-    resize_img = resize_transform(img_loc)
-    transform_img = transform(resize_img)
-    tensor_img = to_tensor(transform_img)
-    normalized_img = normalize_transform(tensor_img)
-
-# Process train/fake folder
-filenames = os.listdir("data/train/fake")
-folder = "data/train/fake"
-
-for filename in filenames:
-    full_path = os.path.join(folder, filename)
-    print(full_path)
-
-    img_loc = Image.open(full_path)
-    resize_img = resize_transform(img_loc)
-    transform_img = transform(resize_img)
-    tensor_img = to_tensor(transform_img)
-    normalized_img = normalize_transform(tensor_img)
-
-# Process test/real folder
-filenames = os.listdir("data/test/real")
-folder = "data/test/real"
-
-for filename in filenames:
-    full_path = os.path.join(folder, filename)
-    print(full_path)
-
-    img_loc = Image.open(full_path)
-    resize_img = resize_transform(img_loc)
-    transform_img = transform(resize_img)
-    tensor_img = to_tensor(transform_img)
-    normalized_img = normalize_transform(tensor_img)
-
-# Process test/fake folder
-filenames = os.listdir("data/test/fake")
-folder = "data/test/fake"
-
-for filename in filenames:
-    full_path = os.path.join(folder, filename)
-    print(full_path)
-
-    img_loc = Image.open(full_path)
-    resize_img = resize_transform(img_loc)
-    transform_img = transform(resize_img)
-    tensor_img = to_tensor(transform_img)
-    normalized_img = normalize_transform(tensor_img)
+# Create transform pipeline using Compose
+transform_pipeline = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.Grayscale(),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.5], std=[0.5])
+])
 
 class SignatureDataset(Dataset):
     def __init__(self, folder_path):
@@ -108,10 +44,7 @@ class SignatureDataset(Dataset):
         # Load image
         img = Image.open(img_path)
 
-        # Apply transformations
-        img = resize_transform(img)
-        img = transform(img)
-        img = to_tensor(img)
-        img = normalize_transform(img)
+        # Apply all transformations at once using pipeline
+        img = transform_pipeline(img)
 
         return img, label
